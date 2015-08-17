01ce55b0344c2500316dcd32415e736536d06cc1
jewel mlnarik <jewel@mlnarik.com>
2015-08-17 10:55:40 -0700

    homepage feature img

--- includes/front-header-image-section.php
+++ includes/front-header-image-section.php
@@ -1,13 +1,11 @@
 <div class="Skt-header-image">
 	<!-- header image -->
-		<div class="advertica-image-post"><img 
-			alt="advertica-default-slider-image" class="ad-slider-image" width="1585"  
-			src="<?php if(sketch_get_option($advertica_shortname.'_frontslider_stype')) { 
-				echo sketch_get_option($advertica_shortname.'_frontslider_stype'); } ?>" ><h1 
-				class="poc-image-hdr"><?php if(sketch_get_option($advertica_shortname.'_logo_alt')) { 
-				echo sketch_get_option($advertica_shortname.'_logo_alt'); } ?><br>
-from the Good News Club</h1><div class="poc-image-subhdr"><div>Make sure you hear both sides of the story about this 
-growing movement to evangelize children in public elementary schools.</div></div>
+		<div class="advertica-image-post" style="background-image:url(<?=sketch_get_option($advertica_shortname.'_frontslider_stype') ?>)">
+			<h1 class="poc-image-hdr"><?=sketch_get_option($advertica_shortname.'_logo_alt') ?><br>
+				from the Good News Club</h1>
+			<div class="poc-image-subhdr"><div>Make sure you hear both sides of the story about this 
+				growing movement to evangelize children in public elementary schools.</div>
+			</div>
 		</div>
 	<!-- end  header image  -->
 </div>
