f7abd93f58b52d89351639aba6f108137e2b9853
server <hello@workfrom.com>
2015-08-23 03:59:46 +0000

    client feedback

--- includes/front-header-image-section.php
+++ includes/front-header-image-section.php
@@ -3,9 +3,9 @@
 		<div class="advertica-image-post" style="background-image:url(<?=sketch_get_option($advertica_shortname.'_frontslider_stype') ?>)">
 			<h1 class="poc-image-hdr"><?=sketch_get_option($advertica_shortname.'_logo_alt') ?><br>
 				from the Good News Club</h1>
-			<div class="poc-image-subhdr"><div>Make sure you hear both sides of the story about this 
-				growing movement to evangelize children in public elementary schools.</div>
-			</div>
+			<div class="poc-image-subhdr"><div>
+			Get the other side of the story about this growing movement to&nbsp;evangelize&nbsp;children in&nbsp;public&nbsp;elementary&nbsp;schools.</div>
+		</div>
 		</div>
 	<!-- end  header image  -->
 </div>
