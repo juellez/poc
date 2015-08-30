875e3c793f1e9284f172b61ae052cdd0c9004031
server <hello@workfrom.com>
2015-08-30 09:17:48 +0000

    sidebar updates made for random quotes

--- sidebar-page.php
+++ sidebar-page.php
@@ -27,12 +27,40 @@ global $advertica_shortname, $post, $sidebar;
 			}
 		?>
 
-		<?php
+                <?php
+                        // get cat of current page
+                        if( $terms = get_the_terms($post->id, 'poc-cat') ){
+                                // create our side bar query
+                                $sbq = new WP_Query( array( 'post_type' => 'sidebar', 'poc-cat' => 'global-1st-block', 'order' => 'random') );
+
+                                if ( $sbq->have_posts() ) : while ( $sbq->have_posts() ) : $sbq->the_post(); ?>
+
+                                <li class="ske-container side-menu"><?php the_content(); ?></li>
+
+                                <?php endwhile; endif;
+                        }
+                ?>
+
+                <?php
+                        // get cat of current page
+                        if( $terms = get_the_terms($post->id, 'poc-cat') ){
+                                // create our side bar query
+                                $sbq = new WP_Query( array( 'post_type' => 'sidebar', 'poc-cat' => 'global-2nd-block', 'order' => 'random') );
+
+                                if ( $sbq->have_posts() ) : while ( $sbq->have_posts() ) : $sbq->the_post(); ?>
+
+                                <li class="ske-container side-menu"><?php the_content(); ?></li>
+
+                                <?php endwhile; endif;
+                        }
+                ?>
+
+		<!-- <?php
 			$i = rand(1,4);
 			if(sketch_get_option($advertica_shortname.'_img'.$i.'_icon')): ?>
 			<li class="ske-container side-menu"><div class="hpblock"><a href="<?= empty( sketch_get_option($advertica_shortname.'_img'.$i.'_link') ) ? '#' : esc_url(sketch_get_option($advertica_shortname.'_img'.$i.'_link','advertica-lite')); ?>" title="<?= empty(sketch_get_option($advertica_shortname.'_img'.$i.'_title')) ? '' : sketch_get_option($advertica_shortname.'_img'.$i.'_title','advertica-lite'); ?>"><img 
 				alt="client-logo" src="<?= empty(sketch_get_option($advertica_shortname.'_img'.$i.'_icon')) ? '' : sketch_get_option($advertica_shortname.'_img'.$i.'_icon','advertica-lite'); ?>"><h4><?= empty(sketch_get_option($advertica_shortname.'_img'.$i.'_title')) ? '' : sketch_get_option($advertica_shortname.'_img'.$i.'_title','advertica-lite'); ?></h4></a></div></li>
-		<?php endif; ?>
+		<?php endif; ?> -->
 
 	</ul>
 </div>
