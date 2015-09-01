da653a29bb7ce7b844356e7cecc752466f3bf7eb
jewel mlnarik <jewel@mlnarik.com>
2015-08-31 22:36:39 -0700

    fix og image default url

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
 	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
 	<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
-	<meta property="og:image" content="/images/protect-oregon-children-homepage.jpg" />
+	<meta property="og:image" content="http://protectoregonchildren.com/images/protect-oregon-children-homepage.jpg" />
 
 <!--[if IE 9]>
 	<meta http-equiv="X-UA-Compatible" content="IE=9" />
