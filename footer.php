aa0a70a5d00899e9d06b89167b98bcd03d338841
jewel mlnarik <jewel@mlnarik.com>
2015-09-04 02:06:57 -0700

    add twitter back to footer along with config options

--- footer.php
+++ footer.php
@@ -29,14 +29,63 @@ if($tweetfeedmeta == '1' || true){ ?>
 	<div id="full-twitter-box">
 		<div class="container">
 			<div class="row-fluid">
-				<!--
+				<?php
+
+				// do we display twitter?
+				if( sketch_get_option($advertica_shortname.'_twitter_handle') ):
+					$twitter_handle = sketch_get_option($advertica_shortname.'_twitter_handle'); // username
+
+					// do we pull in tweets?
+					if( sketch_get_option($advertica_shortname.'_twitter_consumer_key') ){
+						$api_key = urlencode( sketch_get_option($advertica_shortname.'_twitter_consumer_key') );
+						$api_secret = urlencode( sketch_get_option($advertica_shortname.'_twitter_consumer_secret') );
+						$auth_url = 'https://api.twitter.com/oauth2/token'; 
+
+						$data_count = 3; // number of tweets
+						$data_url = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
+
+						// get api access token
+						$api_credentials = base64_encode($api_key.':'.$api_secret);
+
+						$auth_headers = 'Authorization: Basic '.$api_credentials."\r\n".
+						                'Content-Type: application/x-www-form-urlencoded;charset=UTF-8'."\r\n";
+
+						$auth_context = stream_context_create(
+						    array(
+						        'http' => array(
+						            'header' => $auth_headers,
+						            'method' => 'POST',
+						            'content'=> http_build_query(array('grant_type' => 'client_credentials', )),
+						        )
+						    )
+						);
+
+						$auth_response = json_decode(file_get_contents($auth_url, 0, $auth_context), true);
+						$auth_token = $auth_response['access_token'];
+
+						// get tweets
+						$data_context = stream_context_create( array( 'http' => array( 'header' => 'Authorization: Bearer '.$auth_token."\r\n", ) ) );
+
+						$tweets = json_decode(file_get_contents($data_url.'?count='.$data_count.'&screen_name='.urlencode($twitter_handle), 0, $data_context), true);
+					}
+				?>
+				<div class="span4">
 				<img src="/images/twitter-white.png" alt="Twitter" class="footer-twitter" />
-				The latest tweets right here! <span class="footer-twitter-handle">via @protectoregonchildren</span>
-				-->
+				The latest tweets right here <a href="http://twitter.com/<?=$twitter_handle ?>" class="footer-twitter-handle">via @<?=$twitter_handle ?>!</a>
+				</div>
+				<div class="span8" id="footer-twitter-timeline"><?php
+					// result - do what you want
+					for ($i = 1; $i <= 1; $i++) {
+						print('<span><a href="https://twitter.com/badnewsclub/status/'.$tweets[$i]['id'].'" target="_blank">');
+					    print_r($tweets[$i]['text']);
+					    print('</a></span>');
+					}
+				?></div>
+				<?php endif; ?>
 			</div>
 		</div>
 	</div>
-
+	<?php /* var_dump($tweets); */ ?>
 	<div class="third_wrapper">
 		<div class="container">
 			<div class="row-fluid">
@@ -46,7 +95,7 @@ if($tweetfeedmeta == '1' || true){ ?>
 				<div class="footer-list span3 alpha omega"><h4>Get Informed</h4>
 					<a href="/parents">For Parents</a>
 					<br />
-					<a href="/school-administrators">For School Administrators</a>
+					<a href="/administrators">For School Administrators</a>
 					<br />
 					<a href="/resources">Resources</a>
 					<br />
@@ -65,7 +114,7 @@ if($tweetfeedmeta == '1' || true){ ?>
 				</div>
 				<div class="footer-list span2 alpha omega"><h4>Follow Us</h4>
 					<a href="http://facebook.com/protectoregonchildren" title="Facebook"><span class="footer-social sprite-facebook"></a>
-					<!-- <a href="http://twitter.com/protectoregonchildren" title="Twitter"><span class="footer-social sprite-twitter"></a> -->
+					<a href="http://twitter.com/badnewsclub" title="Twitter"><span class="footer-social sprite-twitter"></a>
 				</div>
 
 				<div class="clearfix"></div>
@@ -84,10 +133,27 @@ if($tweetfeedmeta == '1' || true){ ?>
 
 </div>
 <!-- #footer -->
-
 </div>
 <!-- #wrapper -->
 	<a href="JavaScript:void(0);" title="Back To Top" id="backtop"></a>
 	<?php wp_footer(); ?>
+
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+<script>
+// ##### Simple example 1 #####
+// A simple example to get my latest tweet and write to a HTML element with
+// id "example1". Also automatically hyperlinks URLS and user mentions and
+// hashtags.
+var config1 = {
+  "id": '639692570415656960',
+  "domId": 'example1',
+  "maxTweets": 1,
+  "enableLinks": true,
+  "showPermalinks": false
+};
+// twitterFetcher.fetch(config1);
+
+</script>
+
 </body>
 </html>
