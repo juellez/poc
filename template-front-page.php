0b42f14992adc6596b71fb18e706e594f77980e9
jewel <jewel@workfrom.co>
2015-09-20 12:34:38 -0700

    line spacing

--- template-front-page.php
+++ template-front-page.php
@@ -2,7 +2,6 @@
 
 <?php global $advertica_shortname; ?>
 
-<!-- FEATURED BOXES SECTION -->
 <?php include("includes/front-featured-boxes-section.php"); ?>
 
 <!-- PAGE EDITER CONTENT: "home" 10 Questions -->
@@ -23,7 +22,7 @@
 		<!-- h3>Should I send my child to the Good News Club?</h3 -->
 		<div class="cta-main">
 		 <hr class="double" />
-		 <div class="btn-pad"><a class="button main" href="/parents/10-questions/">Ask the Questions</a></div>
+		 <div class="btn-pad"><a class="button main" href="/parents/10-questions/">Get the Questions</a></div>
 		</div>
 	</div>
 </div>
@@ -37,7 +36,7 @@
 	<div class="full-banner">
 		<h2>What is the Good News Club?</h2>
 	</div>
-	<div class="container" style="margin-top: 65px;">
+	<div class="container" style="margin-top: 25px;">
 		<div class="col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1">
 		<?php 
 			$page = get_page_by_path('/home', OBJECT, 'page');
